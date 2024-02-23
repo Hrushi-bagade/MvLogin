1c08916a5207c801bfc0c6ce0cbe1b550e73bdc9
Hrushi-bagade <88029794+Hrushi-bagade@users.noreply.github.com>
2024-02-23 20:03:22 +0530

    Update UserListing.js

--- markas3/src/Component/UserMangement/UserListing.js
+++ markas3/src/Component/UserMangement/UserListing.js
@@ -1,53 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
 import AddUser from "./AddUser";
 import { Table } from "react-bootstrap";
+import { Grid } from "gridjs-react";
 const UserListing = () => {
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
+  const [tableData, setTableData] = useState([{
+    FullName:"",
+    Department:"",
+    Designation: "",
+    MobileNumber:"",
+    EmailId:""
+  }]);
+  useEffect(() => {
+    async function fetchTableData() {
+      try {
+        const response = await fetch(
+          "http://192.168.1.62/MARKAS_WEBAPICORE/api/UserDetail"
+        );
+
+        // Check if the response is OK
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        // Parse the JSON data from the response
+        const data = await response.json();
+
+        console.log("Table Data ", data[0]);
+        setTableData(data);
+      } catch (error) {
+        console.error("Error fetching Table data ", error.message);
+      }
+    }
+    fetchTableData();
+    //  console.log("Table Data ", tableData);
+  }, []);
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Add User
-      </Button>
-      <Table responsive hover className="mb-0">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Job Title</th>
-            <th scope="col">Degree</th>
-            <th scope="col">Salary</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Adrian Monino</td>
-            <td>Front-End Engineer</td>
-            <td>Computer Science</td>
-            <td>$120,000</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Socrates Itumay</td>
-            <td>Software Engineer</td>
-            <td>Computer Engineering</td>
-            <td>$150,000</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Reynante Labares</td>
-            <td>Product Manager</td>
-            <td>Business Management</td>
-            <td>$250,000</td>
-          </tr>
-        </tbody>
-      </Table>
-
-      <AddUser show={show} setShow={setShow} />
+      <div>
+        <div className="mt-5 ml-5 mr-5">
+          <Button variant="primary" className=" mt-10" onClick={handleShow}>
+            Add User
+          </Button>
+          <Grid
+            data={tableData}
+            columns={["ID", "Name", "Job Title", "Degree", "Salary"]}
+            search={true}
+            // pagination={true}
+            sort={true}
+            resizable={true}
+            className={{
+              table: "table table-responsive",
+            }}
+          />
+        </div>
+        <AddUser show={show} setShow={setShow} />
+      </div>
     </>
   );
 };
